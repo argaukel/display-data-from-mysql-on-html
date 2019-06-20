f0be29c69b3aabea143de03a7d006e640d6b176a
arg <anna.gaukel@gmail.com>
2019-06-19 19:02:51 -0500

    tutoring sess

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var mysql = require("mysql");
+var app = express();
+
+
+var data = [
+    {
+        make: 'toyoat',
+        model: 'Camry'
+    },
+    {
+        make: 'ford',
+        model: 'explorer'
+    }
+]
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "todolist"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
+
+
+// html
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+
+// api data
+app.get('/cars', function (req, res) {
+    connection.query("SELECT * FROM todos", function (error, results) {
+        res.send(results)
+    });
+})
+//make route "/remove/:id"
+
+app.listen(3000);
